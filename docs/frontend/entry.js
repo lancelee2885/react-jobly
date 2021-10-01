109e25f77f274258d56c15c7501648a3a6fcf42e
Lance L <lance@Lances-MacBook-Air.local>
2021-10-01 12:38:31 -0700

    folder relocate

---
+++ docs/frontend/entry.js
@@ -0,0 +1,77 @@
+
+    window.reactComponents = {};
+
+    window.vueComponents = {};
+
+  
+      import React from "react";
+
+      import ReactDOM from "react-dom";
+
+
+      import ReactWrapper from '../node_modules/better-docs/lib/react-wrapper.js';
+
+      window.React = React;
+
+      window.ReactDOM = ReactDOM;
+
+      window.ReactWrapper = ReactWrapper;
+
+    
+    import './styles/reset.css';
+
+    import './styles/iframe.css';
+
+  import Component0 from '../src/App.js';
+reactComponents['App'] = Component0;
+
+import Component1 from '../src/CompaniesPage.js';
+reactComponents['CompaniesPage'] = Component1;
+
+import Component2 from '../src/Company.js';
+reactComponents['Company'] = Component2;
+
+import Component3 from '../src/CompanyCard.js';
+reactComponents['CompanyCard'] = Component3;
+
+import Component4 from '../src/CompanyList.js';
+reactComponents['CompanyList'] = Component4;
+
+import Component5 from '../src/Error.js';
+reactComponents['Error'] = Component5;
+
+import Component6 from '../src/Home.js';
+reactComponents['Home'] = Component6;
+
+import Component7 from '../src/Job.js';
+reactComponents['Job'] = Component7;
+
+import Component8 from '../src/JobApplications.js';
+reactComponents['JobApplications'] = Component8;
+
+import Component9 from '../src/JobList.js';
+reactComponents['JobList'] = Component9;
+
+import Component10 from '../src/JobsPage.js';
+reactComponents['JobsPage'] = Component10;
+
+import Component11 from '../src/Loading.js';
+reactComponents['Loading'] = Component11;
+
+import Component12 from '../src/LoginForm.js';
+reactComponents['LoginForm'] = Component12;
+
+import Component13 from '../src/NavBar.js';
+reactComponents['NavBar'] = Component13;
+
+import Component14 from '../src/ProfileForm.js';
+reactComponents['ProfileForm'] = Component14;
+
+import Component15 from '../src/Routes.js';
+reactComponents['Routes'] = Component15;
+
+import Component16 from '../src/SearchForm.js';
+reactComponents['SearchForm'] = Component16;
+
+import Component17 from '../src/SignupForm.js';
+reactComponents['SignupForm'] = Component17;
